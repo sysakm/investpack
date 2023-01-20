a5ee12682a9d3e8e5eeb05fbc43b2c3cdb864308
sysak_ma <sysak.ma@phystech.edu>
2023-01-20 19:44:17 +0300

    Add hk8

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import investpack
 import streamlit as st
 
-tickers = """HKG:0388
+hk_tickers = """HKG:0388
 HKG:3988
 HKG:2331
 HKG:0669
@@ -25,5 +25,18 @@ HKG:9888
 HKG:9626
 HKG:0700
 HKG:1113"""
+
+hk8_tickers = """HKG:0001
+HKG:0175
+HKG:2020
+HKG:2269
+HKG:2628
+HKG:0288
+HKG:0291
+HKG:3888"""
+
 b = investpack.draw_rsi(tickers)
-st.image(b, caption='Main HKG stocks')
+st.image(
+    [investpack.draw_rsi(hk_tickers), investpack.draw_rsi(hk8_tickers)],
+    caption=['Main HKG stocks', 'BX8 HKG stocks']
+)
